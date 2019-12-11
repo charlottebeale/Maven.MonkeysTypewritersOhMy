896b00dcc5475ec96bc919675523eb457fefacbb
Charlotte Beale <charlottebeale@zipcoders-MacBook-Pro-6.local>
2019-12-10 22:09:01 -0500

    Complete

--- src/main/java/io/zipcoder/SafeCopier.java
+++ src/main/java/io/zipcoder/SafeCopier.java
@@ -1,8 +1,31 @@
 package io.zipcoder;
 
+import static java.lang.Thread.sleep;
+import java.util.Random;
+
 /**
  * Make this extend the Copier like `UnsafeCopier`, except use locks to make sure that the actual intro gets printed
  * correctly every time.  Make the run method thread safe.
  */
-public class SafeCopier {
+public class SafeCopier extends Copier{
+    public SafeCopier(String toCopy) {
+        super(toCopy);
+    }
+
+    public void run() {
+        try {
+            while (this.stringIterator.hasNext()) {
+                synchronized (stringIterator) {
+                    if (stringIterator.hasNext()) {
+                        String word = stringIterator.next();
+                        sleep(new Random().nextInt(20));
+                        copied += word + " ";
+
+                    }
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/io/zipcoder/UnsafeCopier.java
+++ src/main/java/io/zipcoder/UnsafeCopier.java
@@ -1,5 +1,7 @@
 package io.zipcoder;
 
+import static java.lang.Thread.sleep;
+
 /**
  * Modify the run function so that the monkeys each grab the next word and write it to the copy.
  */
@@ -9,6 +11,21 @@ public class UnsafeCopier extends Copier {
         super(toCopy);
     }
 
+
     public void run() {
+        try {
+            while (stringIterator.hasNext()) {
+            sleep(100);
+            if(stringIterator.hasNext()) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(this.stringIterator.next()).append(" ");
+                copied += sb.toString();
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }  finally {
+
+        }
     }
 }
